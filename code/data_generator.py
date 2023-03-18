401ef495078d844d4fba33835e71126074a7f336
Coulibaly Zie Mamadou <17972148+zie225@users.noreply.github.com>
2023-03-18 10:33:53 +0100

    Add files via upload

---
+++ code/data_generator.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("all_preprocessed_data.csv")
+
+
+def data_creator():
+    new_df = df.iloc[np.random.randint(0,df.shape[0])]
+    return new_df.to_json()
+    
+
+# for i in range(1):
+#     if __name__ == '__main__':
+#          print(data_creator())
